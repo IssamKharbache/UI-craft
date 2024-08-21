50ba84269233d99a08ffecc604c33a91f1f43888
lefline <issamkharbache2@gmail.com>
2024-08-21 13:15:30 +0100

    finishing component page navbar design

--- components/dashboard/main/componentpage/NavBar.tsx
+++ components/dashboard/main/componentpage/NavBar.tsx
@@ -5,26 +5,13 @@ import { PiPlusCircleDuotone } from 'react-icons/pi';
 
 PiPlusCircleDuotone
 const NavBar = () => {
-  const {showComponentPageObject:{setShowComponentPage,showComponentPage}} = useAppContext();
+
   return (
     <div className='flex items-center  justify-between  mt-4 px-7 mb-4'>
         {/* back button and name */} 
-        <div className="flex items-center gap-4"> 
-           <button className='flex items-center gap-2 border border-red-200 rounded py-2 px-4' onClick={() => setShowComponentPage(false)}>
-            <KeyboardArrowLeftTwoTone />
-            <span>Back</span>
-            </button>
-           
-           <div className="flex flex-col">
-           <span className='font-bold text-xl'>Component name</span>
-           <span className='text-slate-400 text-sm '>Component description</span>
-           </div>
-
-        </div>
-        {/* seach */}
-        <div className="">
-          <input type="text" placeholder='Search...' />
-        </div>
+        <LeftSide />
+        {/* search */}
+         <SearchInput />
         {/* add component button */}
         <button className='flex items-center gap-2 bg-primary text-white px-4 py-2 rounded-md hover:bg-primary-hover transition'>
           <PiPlusCircleDuotone size={25} />
@@ -35,3 +22,31 @@ const NavBar = () => {
 }
 
 export default NavBar
+
+
+//search component
+const SearchInput = () =>{
+return (
+  <div className="">
+          <input type="text" placeholder='Search...' className='py-2 border-2 border-gray-200 px-4 rounded  outline-none focus:border-red-200' />
+        </div>
+)
+}
+//left side component
+const LeftSide = () =>{
+  const {showComponentPageObject:{setShowComponentPage,showComponentPage}} = useAppContext();
+  return(
+    <div className="flex items-center gap-4"> 
+    <button className='flex items-center gap-2 border border-red-200 rounded py-2 px-4' onClick={() => setShowComponentPage(false)}>
+     <KeyboardArrowLeftTwoTone />
+     <span>Back</span>
+     </button>
+    
+    <div className="flex flex-col">
+    <span className='font-bold text-xl'>Component name</span>
+    <span className='text-slate-400 text-sm '>Component description</span>
+    </div>
+
+ </div>
+  )
+}
